5e4b436bab9f4a906657044487b7fe30236a0aa2
Ichchha-coder <ichchhababu1234@gmail.com>
2024-08-22 20:14:42 +0545

    Add venv to .gitignore

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from llama_index.llms.gemini import Gemini
+from document_processor import load_documents_from_web, load_documents_from_directory
+from indexer import create_index
+from chatbot import create_chat_engine
+
+def main():
+    # Initialize LLM
+    llm =Gemini()
+    
+    
+    documents = load_documents_from_directory()
+    
+    # Create index
+    index = create_index(documents, llm)
+    
+    # Create chat engine
+    chat_engine = create_chat_engine(index, llm)
+    
+    print("Hey ready for the talk! Type 'Exit' if you want to stop.")
+    
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'Exit':
+            break
+        
+        response = chat_engine.chat(user_input)
+        print(f"Chatbot: {response}")
+
+if __name__ == "__main__":
+    main()
